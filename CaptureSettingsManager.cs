8bb2bb8ab597831bd67ced72871ff57e4efaf14f
gaboooooon <gaboooooon@gmail.com>
2025-05-24 04:37:15 +0900

    Fix: 保存プロファイルのUnicodeエスケープ回避対応

--- CaptureSettingsManager.cs
+++ CaptureSettingsManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -13,12 +14,24 @@ namespace WowShot2
 
 		private static string ConfigPath => Path.Combine(AppContext.BaseDirectory, "profiles.json");
 
+		//public void Save()
+		//{
+		//	var json = JsonSerializer.Serialize(this, new JsonSerializerOptions
+		//	{
+		//		WriteIndented = true
+		//	});
+		//	File.WriteAllText(ConfigPath, json);
+		//}
+
 		public void Save()
 		{
-			var json = JsonSerializer.Serialize(this, new JsonSerializerOptions
+			var options = new JsonSerializerOptions
 			{
-				WriteIndented = true
-			});
+				WriteIndented = true,
+				Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+			};
+
+			string json = JsonSerializer.Serialize(this, options);
 			File.WriteAllText(ConfigPath, json);
 		}
 
